55342fc7da7454e0d000cfb3305add54d608f9cf
yae0211 <94596959+yae0211@users.noreply.github.com>
2023-02-21 08:48:00 +0100

    Update Image.cpp

--- Archive_Etudiant/Image.cpp
+++ Archive_Etudiant/Image.cpp
@@ -23,25 +23,25 @@ Image::~Image()
 
 Pixel Image::getPix(int &x, int &y)
 {
-    assert(x > 0 && y > 0);
-    assert(x < dimx && y < dimy);
+    assert(x >= 0 && y >= 0);
+    assert(x <= dimx && y <= dimy);
     return tab[y * dimx + x];
 }
 
 void Image::setPix(int &x, int &y, Pixel &couleur)
 {
-    assert(x > 0 && y > 0);
-    assert(x < dimx && y < dimy);
+    assert(x >= 0 && y >= 0);
+    assert(x <= dimx && y <= dimy);
     tab[y * dimx + x] = couleur;
 }
 
 void Image::dessinerRectangle(int Xmin, int Ymin, int Xmax, int Ymax, Pixel &couleur)
 {
-    for (int i = Ymin; i <= Ymax; i++)
+    for (int i = Xmin; i <= Xmax; i++)
     {
-        for (int j = Xmin; i <= Xmax; i++)
+        for (int j = Ymin; i <= Ymax; j++)
         {
-            setPix(j, i, couleur);
+            setPix(i, j, couleur);
         }
     }
 }
@@ -53,7 +53,6 @@ void Image::effacer(Pixel &couleur)
 
 void Image::testRegression()
 {
-
     cout << "## Début du test de régression ##" << endl;
 
     cout << "--- On défini une image par défaut (0*0)" << endl;
@@ -74,9 +73,9 @@ void Image::testRegression()
     assert(im1.dimy == 50);
     cout << ". [V]" << endl;
 
-    cout << "- On vérifie la taille du tableau alloué . ";
-    assert((sizeof(tab) / sizeof(int)) == 500);
-    cout << ". [V]" << endl;
+    //cout << "- On vérifie la taille du tableau alloué . ";
+    //assert((sizeof(tab) / sizeof(int)) == 500);
+    //cout << ". [V] " << endl;
 
     cout << "- On vérifie que le pixel est noir . ";
     for (int i = 0; i < im1.dimx; i++){
@@ -88,11 +87,26 @@ void Image::testRegression()
     }
     cout << ". [V]" << endl;
 
-    cout << "-> On rempli l'image d'une couleur. ";
-    //TODO remplir
-    /*for (int i = 0; i <= (y * x); i++){
-        //TODO assert
-    }*/
+    cout << "- On crées un pixel et on défini ce pixel . ";
+    Pixel px1;
+    px1.setBleu(10);
+    px1.setRouge(15);
+    px1.setVert(20);
+    assert(px1.getBleu() == 10);
+    assert(px1.getRouge() == 15);
+    assert(px1.getVert() == 20);
+    cout << ". [V]" << endl;
+
+    cout << "-> On rempli l'image d'une couleur . ";
+    im1.dessinerRectangle(0, 0, 50, 50, px1);
+    for (int i = 0; i < im1.dimx; i++){
+        for (int j = 0; j < im1.dimy; j++){
+            cout << "PX : " << i << j << endl;
+            assert(im1.getPix(i, j).getRouge() == px1.getRouge());
+            assert(im1.getPix(i, j).getBleu() == px1.getBleu());
+            assert(im1.getPix(i, j).getVert() == px1.getVert());
+        }
+    }
     cout << ". [V]";
 
     cout << "-> On set un pixel spécifique (au pixel par défaut, à un point tiré en random) .";
@@ -100,5 +114,4 @@ void Image::testRegression()
     cout << ". [V]" << endl;
 
     cout << "## Test de régression terminé !! " << endl;
-
 }
