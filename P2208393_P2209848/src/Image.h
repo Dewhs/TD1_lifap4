b9ab4d809abb02dea16bda9f5a99fc00f2ff8a16
yae0211 <94596959+yae0211@users.noreply.github.com>
2023-02-23 16:57:44 +0100

    Doxygen update

--- P2208393_P2209848/src/Image.h
+++ P2208393_P2209848/src/Image.h
@@ -18,51 +18,80 @@ private:
     unsigned int dimx, dimy; // les dimensions de l'image
     Pixel* tab; // le tableau 1D de pixel
 public:
-    // Constructeur par défaut de la classe : initialise à 0
-    // ce constructeur n'alloue pas de pixel
+
+    /** Constructeur d'une image par défaut
+    */
     Image();
-    // Constructeur de la classe : initialise selon dimensionX et dimensionY
-    // puis alloue le tableau de pixel dans le tas (image noire)
+    /** Constructeur d'une image avec une dimension précise
+    * @param dimensionX dimension sur l'axe x de l'image
+    * @param dimensionY dimension sur l'axe y de l'image
+    */
     Image(int dimensionX, int dimensionY);
 
-    // Destructeur de la classe : désallocation de la mémoire du tableau de pixels
-    // et mise à jour des champs dimx et dimy à 0
+    /** Détruit l'image
+    */
     ~Image();
 
-    // Accesseur : récupère le pixel original de coordonnées (x,y) en vérifiant leur validité
+    /** Accesseur : Retourne le pixel de coordonnées (x, y) de l'image
+    * @param x La coordonnée x du pixel
+    * @param y La coordonnée y du pixel
+    * @return Le pixel demandé
+    */
     Pixel getPix(const unsigned int &x, const unsigned int &y) const;
 
-    // Mutateur : modifie le pixel de coordonnée (x, y)
+    /** Mutateur : Défini la valeur d'un pixel de l'image
+    * @param x La coordonnée x du pixel
+    * @param y La coordonnée y du pixel
+    * @param couleur Le pixel couleur souhaité
+    */
     void setPix(const unsigned int& x,const unsigned int& y, Pixel& couleur);
 
-    // Dessine un rectangle plein de la couleur de l'image
+    /** Permet de remplir une image pixel par pixel de façon rectangulaire par une couleur (un pixel) défini
+    * @param Xmin La coordonnée x du premier pixel
+    * @param Ymin La coordonnée y du premier pixel
+    * @param Xmax La coordonnée x du dernier pixel
+    * @param Ymax La coordonnée y du dernier pixel
+    * @param couleur Le pixel couleur souhaité
+    */
     void dessinerRectangle (unsigned int Xmin,unsigned int Ymin,unsigned int Xmax,unsigned int Ymax, Pixel& couleur);
 
-    // Efface l'image en la remplissant de la couleur en paramètre
+    /** Permet d'effacer (ou de remplir) entièrement une image par un pixel couleur spécifié
+    * @param couleur Le pixel couleur souhaité
+    */
     void effacer(Pixel& couleur);
 
-    //Sauvegarde une image dans un fichier
+    /** Permet de sauvegarder une image en fichier .ppm
+    * @param filename La destination du fichier souhaité
+    */
     void sauver(const string & filename) const;
 
-    //Ouvre une image depuis un fichier
+    /** Permet d'ouvrir une image depuis un fichier .ppm
+    * @param filename Le chemin du fichier source
+    */
     void ouvrir(const string & filename);
 
-    //Affiche une image dans la console
+    /** Permet d'afficher l'image en console
+    */
     void afficherConsole();
 
-    //SDL : Permet l'initialisation de l'affichage de l'image.
+    /** Initialise l'affichage SDL
+    */
     void afficherInit();
 
-    //SDL : Permet la gestion des évènements de l'image (zoom/désoom)
+    /** Gère les évènements de l'image SDL
+    */
     void afficherBoucle();
 
-    //SDL : Permet de détruire une image affichée
+    /** Détruit l'image SDL
+    */
     void afficherDetruit();
 
-    //SDL : Permet l'affichage d'une image
+    /** Permet d'afficher l'image en mode graphique avec SDL
+    */
     void afficher();
 
-    // Effectue une série de tests vérifiant que le module fonctionne et que les données membres de l'objet sont conformes
+    /** Effectue une série de tests sur toutes les fonctions de Image.cpp, vérifie le bon fonctionnement des fonctions et vérifie que les données sont conformes
+    */
     void testRegression();
 
 };
--- P2208393_P2209848/src/Pixel.cpp
+++ P2208393_P2209848/src/Pixel.cpp
@@ -1,7 +1,5 @@
 #include "Pixel.h"
 
-/** Constructeur d'un pixel avec des valeurs par défaut
- */
 Pixel::Pixel()
 {
     r = 0;
@@ -10,67 +8,39 @@ Pixel::Pixel()
 
 }
 
-/** Constructeur d'un pixel avec des valeurs définies
- * @param nr La composante rouge
- * @param ng La composante verte
- * @param nb La composante bleu
- */
 Pixel::Pixel(unsigned char nr, unsigned char ng, unsigned char nb) : r(nr), g(ng), b(nb)
 {
 }
 
-/** Destructeur d'un pixel
- */
 Pixel::~Pixel() {
     
 }
 
-/** Accesseur de la composante rouge
- * @return récupère la composante rouge du pixel
- */
 unsigned char Pixel::getRouge() const
 {
     return r;
 }
 
-/** Accesseur de la composante verte
- * @return récupère la composante verte du pixel
- */
 unsigned char Pixel::getVert() const
 {
     return g;
 }
 
-/** Accesseur de la composante bleue
- * @return récupère la composante bleue du pixel
- */
 unsigned char Pixel::getBleu() const
 {
     return b;
 }
 
-/** Mutateur de la composante rouge
- * @param nr composante rouge souhaité
- * @return défini la composante rouge du pixel
- */
 void Pixel::setRouge(unsigned char nr)
 {
     r = nr;
 }
 
-/** Mutateur de la composante verte
- * @param ng composante verte souhaité
- * @return défini la composante verte du pixel
- */
 void Pixel::setVert(unsigned char ng)
 {
     g = ng;
 }
 
-/** Mutateur de la composante bleue
- * @param nb composante bleue souhaité
- * @return défini la composante bleue du pixel
- */
 void Pixel::setBleu(unsigned char nb)
 {
     b = nb;
