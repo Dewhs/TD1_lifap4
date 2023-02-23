448aa0cb70d7e12ac092359bb3496be50aecc759
yae0211 <94596959+yae0211@users.noreply.github.com>
2023-02-23 16:13:52 +0100

    comment pixel.cpp

--- P2208393_P2209848/src/Pixel.cpp
+++ P2208393_P2209848/src/Pixel.cpp
@@ -2,49 +2,81 @@
 
 Pixel::Pixel()
 {
-    int z = 0;
-    r = z;
-    g = z;
-    b = z;
+    r = 0;
+    g = 0;
+    b = 0;
 
 }
 
-// Constructeur de la classe: initialise r,g,b avec les paramètresL
+/**
+ * Constructeur d'un pixel
+ * @param nr La composante rouge
+ * @param ng La composante verte
+ * @param nb La composante bleu
+ */
 Pixel::Pixel(unsigned char nr, unsigned char ng, unsigned char nb) : r(nr), g(ng), b(nb)
 {
 }
 
+/**
+ * Destructeur d'un pixel
+ */
 Pixel::~Pixel() {
     
 }
 
-// Accesseur : récupère la composante rouge du pixel
+/**
+ * Accesseur de la composante rouge
+ * @return récupère la composante rouge du pixel
+ */
 unsigned char Pixel::getRouge() const
 {
     return r;
 }
-// Accesseur : récupère la composante verte du pixel
+
+/**
+ * Accesseur de la composante verte
+ * @return récupère la composante verte du pixel
+ */
 unsigned char Pixel::getVert() const
 {
     return g;
 }
-// Accesseur : récupère la composante bleue du pixel
+
+/**
+ * Accesseur de la composante bleue
+ * @return récupère la composante bleue du pixel
+ */
 unsigned char Pixel::getBleu() const
 {
     return b;
 }
 
-// Mutateur : modifie la composante rouge du pixel
+/**
+ * Mutateur de la composante rouge
+ * @param nr composante rouge souhaité
+ * @return défini la composante rouge du pixel
+ */
 void Pixel::setRouge(unsigned char nr)
 {
     r = nr;
 }
-// Mutateur : modifie la composante verte du pixel
+
+/**
+ * Mutateur de la composante verte
+ * @param ng composante verte souhaité
+ * @return défini la composante verte du pixel
+ */
 void Pixel::setVert(unsigned char ng)
 {
     g = ng;
 }
-// Mutateur : modifie la composante bleue du pixel
+
+/**
+ * Mutateur de la composante bleue
+ * @param nb composante bleue souhaité
+ * @return défini la composante bleue du pixel
+ */
 void Pixel::setBleu(unsigned char nb)
 {
     b = nb;
